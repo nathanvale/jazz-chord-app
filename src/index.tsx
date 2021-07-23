3ea879f1b34fa828e0856b175603e58dccad95b3
Nathan Vale <hi@nathanvale.com>
2021-07-23 15:17:39 +1000

    chore: wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import netlifyIdentity from "netlify-identity-widget";
 
 import App from "./App";
+
+(window as any).netlifyIdentity = netlifyIdentity;
+// You must run this once before trying to interact with the widget
+netlifyIdentity.init();
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
